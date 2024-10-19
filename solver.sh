0728ad7c9e18e83e02fbd274fbdc7c9f38492637
Shotaro Osaki <1890577+e9@users.noreply.github.com>
2024-10-20 01:32:28 +0900

    update

--- solver.sh
+++ solver.sh
@@ -1,6 +1,6 @@
 source "/home/e9/emscripten-core/emsdk/emsdk_env.sh"
 emcc source/wasm.c source/shtsume.c source/ntbase.c source/nbase.c source/base.c  source/ncheck.c source/nusi.c source/usi.c source/nsearchinf.c  source/bitboard.c source/global.c source/nevasion.c  source/ndtools.c source/dtools.c \
--s ALLOW_MEMORY_GROWTH \
+-s INITIAL_MEMORY=268435456 \
 -s MODULARIZE=1 \
 -s ENVIRONMENT='worker' \
 -s EXPORTED_FUNCTIONS="['_solve']" \
